27d7ffea3908b797af474ba9b45d946af86e852f
Mehdi Hadeli <mehdihadeli@gmail.com>
2024-11-02 17:38:30 +0330

    feat: :sparkles: add samples for highlighting

--- samples/console-output/csharp-console-highlighter/Program.cs
+++ samples/console-output/csharp-console-highlighter/Program.cs
@@ -2,35 +2,6 @@
 using SyntaxHighlighter.Lexers;
 using SyntaxHighlighter.Styles;
 
-string goSourceCode =
-    @"```go
-package main
-
-import (
-	""fyne.io/fyne/v2/app""
-	""fyne.io/fyne/v2/container""
-	""fyne.io/fyne/v2/widget""
-)
-
-func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow(""Opacity Example"")
-
-    int i:= 1
-
-	// Create a container with a semi-transparent background
-	content := container.NewVBox(
-		widget.NewLabel(""This is some text with a semi-transparent background""),
-	)
-
-	// Setting a custom background color with opacity
-	content.SetBackgroundColor(fyne.NewColor(255, 0, 0, 128)) // RGBA
-
-	myWindow.SetContent(content)
-	myWindow.ShowAndRun()
-}
-```";
-
 string csharpSourceCode =
     @"```csharp
 // This is a single-line comment
---
+++ samples/console-output/golang-console-highlighter/Program.cs
@@ -0,0 +1,42 @@
+﻿using SyntaxHighlighter.Formatters;
+using SyntaxHighlighter.Lexers;
+using SyntaxHighlighter.Styles;
+
+string goSourceCode =
+    @"```go
+package main
+
+import (
+	""fyne.io/fyne/v2/app""
+	""fyne.io/fyne/v2/container""
+	""fyne.io/fyne/v2/widget""
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow(""Opacity Example"")
+
+    int i:= 1
+
+	// Create a container with a semi-transparent background
+	content := container.NewVBox(
+		widget.NewLabel(""This is some text with a semi-transparent background""),
+	)
+
+	// Setting a custom background color with opacity
+	content.SetBackgroundColor(fyne.NewColor(255, 0, 0, 128)) // RGBA
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+}
+```";
+
+Lexer lexer = new GoLexer();
+Formatter formatter = new ConsoleFormatter256();
+var style = await Style.LoadFromJsonAsync("dracula.json");
+
+var tokens = lexer.Tokenize(goSourceCode);
+formatter.Format(tokens, Console.Out, style);
+Console.WriteLine(); 
+
+Console.ReadKey();
